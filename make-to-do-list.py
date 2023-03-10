96435713a4909e9c6e818803fe0c4ebb686d4913
Haile Terry <BYU\hterry17@hikari.byu.edu>
2023-03-10 14:32:24 -0700

    Removed some extra comments, cleaned up code, etc

--- make-to-do-list.py
+++ make-to-do-list.py
@@ -2,47 +2,34 @@ from glob import glob
 import os
 from bs4 import BeautifulSoup
 
-# helper function to write html to file
 def writeHTML(fileName, html):
     f = open(fileName, "w+")
     f.write(html)
     f.close()
 
 if __name__ == '__main__':
-    # get list of files in no-record folder
     noRecordFiles = glob("/storage/jeremy/imf/exec-archives/html/other/no-record/*.html")
-    # print(noRecordFiles)
 
-    # for each file in no-record folder
-    for file in noRecordFiles:
-        # print(file)
-        # open file
+    for file in noRecordFiles:        
         f = open(file, "r")
-        # read file
         html = f.read()
-        # print(html)
-        # parse for record and hierarchy
+        f.close()
+
         soup = BeautifulSoup(html, 'html.parser')
         title = soup.find("title").text.strip()
-        # if (title != "Request Rejected" and title.count("Simple search") == 0):
-        #     print("\n", file)
-        #     print("TITLE:", title)
+        num = file.split("/")[-1]
+        fileName = "/storage/jeremy/imf/exec-archives/html/other/"
 
         if title == "Request Rejected":
-            num = file.split("/")[-1]
-            fileName = "/storage/jeremy/imf/exec-archives/html/other/request-rejected/" + num
-            writeHTML(fileName, html)
-            os.remove(file)
+            fileName += "request-rejected/"
         elif "Simple search" in title:
-            num = file.split("/")[-1]
-            fileName = "/storage/jeremy/imf/exec-archives/html/other/temp/" + num
-            writeHTML(fileName, html)
-            os.remove(file)
+            fileName += "temp/"
         else:
-            num = file.split("/")[-1]
-            fileName = "/storage/jeremy/imf/exec-archives/html/other/unknown-error/" + num
-            writeHTML(fileName, html)
-            os.remove(file)
+            fileName += "unknown-error/"
+            
+        fileName += num
+        writeHTML(fileName, html)
+        os.remove(file)
 
         # if no record try again 10 times before giving up
         # check for duplicate files due to multiple processes running
--- scraper-imf-exec-archives.py
+++ scraper-imf-exec-archives.py
@@ -94,12 +94,9 @@ if __name__ == '__main__':
         brokenTracker = 0
         sleep (1)
 
-        #less file, inside 'F'
-        #come back to 125061624 - 125065837
+        # less file, inside 'F'
+        # come back to 125061624 - 125065837
 
-        # from glob import glob
-        # give it the path /storage/jeremy/imf/exec-archives/html/other/no-record/
-        #python os module - delete a file
         # if no record try again 10 times before giving up
 
         # check for duplicate files due to multiple processes running
