6281680fe98d32db874faa6f8faadea02ea8418a
Haile Terry <BYU\hterry17@hikari.byu.edu>
2023-03-10 12:56:10 -0700

    Created file to attempt to fix the bad file structures resulting from server errors and bad HTML responses

---
+++ make-to-do-list.py
@@ -0,0 +1,17 @@
+# helper function to write html to file
+def writeHTML(fileName, html):
+    f = open(fileName, "w+")
+    f.write(html)
+    f.close()
+
+if __name__ == '__main__':
+
+        #less file, inside 'F'
+        #come back to 125061624 - 125065837
+
+        # from glob import glob
+        # give it the path /storage/jeremy/imf/exec-archives/html/other/no-record/
+        #python os module - delete a file
+        # if no record try again 10 times before giving up
+
+        # check for duplicate files due to multiple processes running
