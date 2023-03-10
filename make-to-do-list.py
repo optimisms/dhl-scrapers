f1e051bdbeddefd23d80e7e0a0e0e7a3e2b21d1d
Haile Terry <BYU\hterry17@hikari.byu.edu>
2023-03-10 14:14:27 -0700

    Wrote script to move Rejected Requests to new directory

--- make-to-do-list.py
+++ make-to-do-list.py
@@ -1,3 +1,7 @@
+from glob import glob
+import os
+from bs4 import BeautifulSoup
+
 # helper function to write html to file
 def writeHTML(fileName, html):
     f = open(fileName, "w+")
@@ -5,9 +9,50 @@ def writeHTML(fileName, html):
     f.close()
 
 if __name__ == '__main__':
+    # get list of files in no-record folder
+    noRecordFiles = glob("/storage/jeremy/imf/exec-archives/html/other/no-record/*.html")
+    # print(noRecordFiles)
 
-        #less file, inside 'F'
-        #come back to 125061624 - 125065837
+    # for each file in no-record folder
+    for file in noRecordFiles:
+        # print(file)
+        # open file
+        f = open(file, "r")
+        # read file
+        html = f.read()
+        # print(html)
+        # parse for record and hierarchy
+        soup = BeautifulSoup(html, 'html.parser')
+        title = soup.find("title").text.strip()
+        # if (title != "Request Rejected" and title.count("Simple search") == 0):
+        #     print("\n", file)
+        #     print("TITLE:", title)
+
+        if title == "Request Rejected":
+            print(file)
+            title = file.split("/")[-1]
+            fileName = "/storage/jeremy/imf/exec-archives/html/other/request-rejected/" + title
+            writeHTML(fileName, html)
+            # delete file
+            os.remove(file)
+        
+
+        # break
+        # record = soup.find(class_='record')
+        # hierarchy = soup.find(class_='hierarchy')
+
+        # if no record, save in special folder and continue to next iteration
+        # if record == None:
+        #     htmlName = "/storage/jeremy/imf/exec-archives/html/other/no-record/" + incToString(incremental) + ".html"
+        #     writeHTML(htmlName, soup.prettify())
+        #     brokenTracker += 1
+
+        #     # if broken for 50 iterations, print debug statements and stop
+        #     if brokenTracker >= 50:
+        #         print("Broken for 50 iterations, stopping on " + incToString(incremental))
+        #         print("Total reqs before stopping: " + str(totalReqs))
+        #         break
+        #     continue
 
         # from glob import glob
         # give it the path /storage/jeremy/imf/exec-archives/html/other/no-record/
